c40ddfa8f1416696f81598dbe83fb7ef89ef84c7
jonaselmlund <jonas.elmlund@gmail.com>
2025-03-10 10:19:56 +0100

    add

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hello World!!!</Text>
+      <Text>Välkommen!</Text>
+      <Text>Eftersom det är första gången du öppnar Teamradar behöver du ange ditt namn.</Text>
     </View>
   );
 }
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # teamradar
+
+#GIT
+git config --global user.name "jonaselmlund"
+git config --global user.email "jonas.elmlund@gmail.com"
+
+#hur man kör servern:
+npx expo start 
+
+#Info om react native-appen.
+App.js är startsidan.
