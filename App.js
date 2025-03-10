5d2a94916d68ea801e1ec28c98553f2f2d582e9e
jonaselmlund <jonas.elmlund@gmail.com>
2025-03-10 09:57:25 +0100

    test

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hello World!!</Text>
+      <Text>Hello World!!!</Text>
     </View>
   );
 }
