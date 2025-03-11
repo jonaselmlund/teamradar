2790206bf074e55400d0f64a2a90a29581eda370
jonaselmlund <jonas.elmlund@gmail.com>
2025-03-11 16:53:14 +0100

    bort ignore firebase firebaseConfig

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,15 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// 🔥 DINA FIREBASE-KONFIGURATIONSUPPGIFTER HÄMTAS FRÅN FIREBASE-KONSOLEN 🔥
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyD7vhdeLqD2iLDmQ56bxE1nzH9C3NTJstE",
-    authDomain: "teamradar-c118e.firebaseapp.com",
-    projectId: "teamradar-c118e",
-    storageBucket: "teamradar-c118e.firebasestorage.app",
-    messagingSenderId: "293037580610",
-    appId: "1:293037580610:web:fd2504cc8e1c6c666068db"
-  };
+  apiKey: "AIzaSyD7vhdeLqD2iLDmQ56bxE1nzH9C3NTJstE",
+  authDomain: "teamradar-c118e.firebaseapp.com",
+  projectId: "teamradar-c118e",
+  storageBucket: "teamradar-c118e.firebasestorage.app",
+  messagingSenderId: "293037580610",
+  appId: "1:293037580610:web:7dc37a0aed470e0c6068db"
+};
 
 // Initiera Firebase
 const app = initializeApp(firebaseConfig);
